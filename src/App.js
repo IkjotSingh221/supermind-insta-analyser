af5ddef6ccde93925f615503e1e4ca4150c06f6d
ASUS <khattar.harshita242@gmail.com>
2025-01-05 01:23:28 +0530

    dashboard initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import Dashboard from "./UI/dashboard/Dashboard";
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Router> 
+      <div className="app-container">
+        <div className="content-container">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          
+          
+        </Routes>
+        </div>
+        
+      </div>
   );
 }
 
-export default App;
+export default function AppWrapper() {
+  return(
+    <Router>
+      <App/>
+    </Router>
+  );
+}
